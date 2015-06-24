94c6e3415eeb0d2b9a315441c136ab89e4bc3f25
KateWilkinson <kate.wilkinson@hotmail.com>
2015-06-24 10:20:45 +0100

    Temperature reporter

--- lib/city_temperature_reporter.rb
+++ lib/city_temperature_reporter.rb
@@ -1,4 +1,16 @@
-class TemperatureReporter
+class CityTemperatureReporter
+
+  attr_reader :city_name, :weatherman
+
+  def initialize city_name, weatherman
+    @city_name = city_name
+    @weatherman = weatherman
+  end
+
+  def temperature
+    weatherman.get_temperature city_name
+  end
+
 
 
 end
--- spec/city_temperature_reporter_spec.rb
+++ spec/city_temperature_reporter_spec.rb
@@ -1,13 +1,25 @@
 require './lib/city_temperature_reporter'
 
-describe TemperatureReporter do
+describe CityTemperatureReporter do
 
   it 'requests its current temperature' do
-    # think about what TemperatureReporter needs to do its job
-    # How can you test that it makes this request? Use diagrams to help you visualise this
+    #SETUP
+    weatherman = double :weatherman
+    new_york   = CityTemperatureReporter.new('New York', weatherman)
+    #EXPECTATION ABOUT THE FUTURE
+    expect(weatherman).to receive(:get_temperature).with('New York')
+    #ACTION
+    new_york.temperature
+
   end
 
   it 'reports its current temperature' do
-    # once it's got that temperature, can it report it?
+    #SETUP
+    weatherman = double :weatherman
+    allow(weatherman).to receive(:get_temperature).with('Seoul').and_return('29')
+    seoul      = CityTemperatureReporter.new('Seoul', weatherman)
+    #EXPECTATION
+    expect(seoul.temperature).to eq '29'
+
   end
 end
